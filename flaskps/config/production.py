458041d55cfb4039b66806f38db9ffdbf8ecf086
manurtinez <mantinez@gmail.com>
2019-10-10 12:28:45 -0300

    subo config de db

---
+++ flaskps/config/production.py
@@ -0,0 +1,6 @@
+SECRET_KEY = '\xa6\xbaG\x80\xc9-$s\xd5~\x031N\x8f\xd9/\x88\xd0\xba#B\x9c\xcd_'
+DEBUG = False
+DB_HOST = 'localhost'
+DB_USER = 'grupo36'
+DB_PASS = 'ZjVmMDRmZTczOTQ4'
+DB_NAME = 'grupo36'
