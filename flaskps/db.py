84f3f690535f8b26a2483dcac0a7245524766c25
manurtinez <mantinez@gmail.com>
2019-10-15 23:42:16 -0300

    agrego cositas de prueba

---
+++ flaskps/db.py
@@ -0,0 +1,16 @@
+import pymysql
+
+from flask import current_app, g
+from flask.cli import with_appcontext
+from flaskps.config import Config
+
+def get_db():
+    if 'db' not in g:
+        g.db = pymysql.connect(
+            host=Config.DB_HOST,
+            user=Config.DB_USER,
+            password=Config.DB_PASS,
+            db=Config.DB_NAME,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    return g.db
