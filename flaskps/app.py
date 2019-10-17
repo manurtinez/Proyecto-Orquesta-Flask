e87abea0ea8968a3c107ba2907f4b2083659eb4c
manurtinez <mantinez@gmail.com>
2019-10-17 11:34:55 -0300

    login y logout (no funca)

--- flaskps/app.py
+++ flaskps/app.py
@@ -1,5 +1,5 @@
 from os import path
-from flask import Flask, render_template, g, request, session, flash
+from flask import Flask, url_for, render_template, g, request, session, flash, redirect
 
 app = Flask(__name__)
 app.config.from_pyfile('config/production.py')
@@ -16,19 +16,24 @@ app.config.from_pyfile('config/production.py')
 
 @app.route('/')
 def index():
-    if not session.get('logged_in'):
-        return render_template('inicio.html')
+    return render_template('inicio.html')
 
 @app.route('/login.html', methods=['POST', 'GET'])
 def login():
+    error = ''
     if request.method == 'POST':
         if request.form['password'] == '1234' and request.form['user'] == 'admin':
             session['logged_in'] = True
+            flash('logeo exitoso!')
             return redirect(url_for('index'))
         else:
-            flash('noppp')
-            return index()
-    return render_template('login.html')
+            error = 'credenciales invalidas'
+    return render_template('login.html', error=error)
+
+@app.route('/')
+def logout():
+    session['logged_in'] = False
+    return index()
 
 if __name__ == '__main__': 
   
