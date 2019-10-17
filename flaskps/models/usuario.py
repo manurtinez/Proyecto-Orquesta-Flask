e61d2c9e42fb097002e1df7985838e421b4b2b3d
Oliveras, Ailén <ailen_oliveras7@hotmail.com>
2019-10-17 12:28:21 -0300

    actualziacion modulo de consultas de la bd

--- flaskps/models/usuario.py
+++ flaskps/models/usuario.py
@@ -1,5 +1,5 @@
-class usuario(object)
-db = None
+class usuario(object):
+    db = None
 
     @classmethod
     def all(cls):
@@ -11,18 +11,18 @@ db = None
         return cursor.fetchall()
 
     @classmethod
-    def create(cls, data):
+    def create(cls, em, us, pa, ac, up, cr, fi, la): #1° no esten vacios (verificar desde el controlador, resources > usuario.py).
         sql = """
             INSERT INTO usuario (email, username, password, activo, updated_at, created_at, first_name, last_name )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            VALUES (em, us, pa, ac, up, cr, fi, la) 
         """
-
         cursor = cls.db.cursor()
-        cursor.execute(sql, list(data.values()))
+        cursor.execute(sql)
         cls.db.commit()
 
         return True
 
+    @classmethod
     def select_activos(cls):
         sql = "SELECT * FROM usuario WHERE activo = 1"
 
@@ -30,3 +30,14 @@ db = None
         cursor.execute(sql)
 
         return cursor.fetchall()
+
+    @classmethod
+    def find_by_email_and_pass(cls,e,p):
+        sql = "SELECT * FROM usuario WHERE email = e AND password = p" 
+        #asegurar que compare con las variables, no con el string
+
+        cursor = cls.db.cursor()
+        resultado = cursor.execute(sql) 
+        return resultado
+
+#por cada metodo va su equivalente en el controlador, ahi aseguro q no este vacio. redirigir desde el mismo controlador.
