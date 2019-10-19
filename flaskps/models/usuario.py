540b812897c5e56f9145eb2a0c38aa7dde47c1d7
Oliveras, Ailén <ailen_oliveras7@hotmail.com>
2019-10-19 00:34:46 -0300

    actualizacion de las consultas del usuario (acceso a parametros)

--- flaskps/models/usuario.py
+++ flaskps/models/usuario.py
@@ -14,7 +14,7 @@ class usuario(object):
     def create(cls, em, us, pa, ac, up, cr, fi, la): #1° no esten vacios (verificar desde el controlador, resources > usuario.py).
         sql = """
             INSERT INTO usuario (email, username, password, activo, updated_at, created_at, first_name, last_name )
-            VALUES (em, us, pa, ac, up, cr, fi, la) 
+            VALUES (@em, @us, @pa, @ac, @up, @cr, @fi, @la) 
         """
         cursor = cls.db.cursor()
         cursor.execute(sql)
@@ -33,7 +33,7 @@ class usuario(object):
 
     @classmethod
     def find_by_email_and_pass(cls,e,p):
-        sql = "SELECT * FROM usuario WHERE email = e AND password = p" 
+        sql = "SELECT * FROM usuario WHERE email = @e AND password = @p" 
         #asegurar que compare con las variables, no con el string
 
         cursor = cls.db.cursor()
