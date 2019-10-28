eb5015e1103cb2527ce2dbd201bd20c2caf1bd31
hugocontrera <dario_contrera.miranda@hotmail.com>
2019-10-28 20:42:03 -0300

    cambios

--- flaskps/resources/admin.py
+++ flaskps/resources/admin.py
@@ -1,7 +1,43 @@
-# from os import path
-# from flask import Flask, url_for, render_template, g, request, session, flash, redirect
-# from flaskps.models.usuario import usuario
-# from flaskps.db import get_db
-# from flaskps import app
+from flask import redirect, render_template, request, session, url_for, flash
+from flaskps.db import db
+#from flaskps.models.configuracion import Configuracion
 
+def administracion():
+     return render_template('administracion.html')
 
+#desactivar el la pagina web FALTA desactivar los templates y agregar la opcion activar sitio
+#@app.route('/desactivar.html')
+#def desactivar():
+#    return render_template('desactivar.html')
+
+#informacion de la orquesta LISTO!!
+
+def informacion():
+    #cur = mysql.connection.cursor()
+    #cur.execute('SELECT * FROM configuracion')
+    #data = cur.fetchall()
+    return render_template('informacion.html')
+
+#listar los elementos de las pag FALTA implementar
+#@app.route('/listar.html')
+#def listar():
+#    return render_template('listar.html')
+
+#formulario para editar informacion
+
+def formulario():
+ #    cur = mysql.connection.cursor()
+ #    cur.execute('SELECT * FROM configuracion WHERE id = %s', (id))
+ #    data = cur.fetchall()
+     return render_template('formulario.html')
+
+ #@app.route('/editar/<id>', methods = ['POST'])
+ #def update_conf(id):
+ #    if request.method == 'POST':
+ #        titulo = request.form['titulo']
+ #        descripcion = request.form['descripcion']
+ #        mail = request.form['mail']
+ #        cur = mysql.connection.cursor()
+ #        cur.execute('UPDATE configuracion SET titulo = %s, descripcion = %s, mail = %s WHERE id = %s', (titulo, descripcion, mail, id))
+ #        mysql.connection.commit()
+ #       return redirect(url_for('administracion.html'))
