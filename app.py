f6f0988eb38b5b56f335769d0cb9679792d57aca
manurtinez <mantinez@gmail.com>
2019-11-11 12:36:14 -0300

    hago que las altas se hagan con modals

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ from flask import session
 from flask_session import Session
 
 if __name__ == '__main__':
-    app.config['SESSION_TYPE'] = 'filesystem'
-    Session(app)
     session.clear()
     app.run()
