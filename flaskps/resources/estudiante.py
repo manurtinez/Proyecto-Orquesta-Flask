cf8728129b96458b6f59492eb8984a62738d79d4
manurtinez <mantinez@gmail.com>
2019-11-12 18:40:07 -0300

    completo los selects de alta de estudiante

--- flaskps/resources/estudiante.py
+++ flaskps/resources/estudiante.py
@@ -1,9 +1,10 @@
-from flask import render_template, abort, url_for, request, redirect, session, flash
+from flask import render_template, jsonify, abort, url_for, request, redirect, session, flash
 from flaskps.models import configuracion
 from flaskps.models.escuela import Escuela
 from flaskps.models.nivel import Nivel
 from flaskps.models.usuario import User
 from flaskps.models.barrio import Barrio
+import json, requests
 
 
 def listadoEstudiantes():
@@ -16,4 +17,16 @@ def listadoEstudiantes():
     return render_template('user/listado.html', lista=lista, cant=tabla.cantListar)
 
 def altaEstudiante():
-    return render_template('/estudiante/altaEstudiante.html', escuelas=Escuela.get_all(), niveles=Nivel.get_all(), barrios=Barrio.get_all())
+    tiposDNI = []
+    listaLoc = []
+    dnis = requests.get('https://api-referencias.proyecto2019.linti.unlp.edu.ar/tipo-documento')
+    localidades = requests.get('https://api-referencias.proyecto2019.linti.unlp.edu.ar/localidad')
+    dnis = json.loads(dnis.text)
+    localidades = json.loads(localidades.text)
+    for i in range(len(dnis)):
+        tiposDNI.append(dnis[i]['nombre'])
+    for i in range(len(localidades)):
+        listaLoc.append(localidades[i]['nombre'])
+    print(tiposDNI)
+    return render_template('/estudiante/altaEstudiante.html', escuelas=Escuela.get_all(), niveles=Nivel.get_all(),
+     barrios=Barrio.get_all(), dnis=tiposDNI, localidades=listaLoc)
