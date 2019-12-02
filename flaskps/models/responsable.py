413e0c2fa65d5e83becae6dc4ad53ae8efdccd47
manurtinez <mantinez@gmail.com>
2019-12-02 01:13:23 -0300

    ME COSTO HORAS pero agrego la funcionalidad de nuevo responsable

--- flaskps/models/responsable.py
+++ flaskps/models/responsable.py
@@ -14,9 +14,17 @@ class Responsable(db.Model):
     genero_id = db.Column(db.Integer)
     tipo_doc_id = db.Column(db.Integer)
     numero = db.Column(db.Integer)
+    is_deleted = db.Column(db.Boolean)
 
     def all():
         return Responsable.query.all()
 
     def getById(id):
         return Responsable.query.filter_by(id=id).first()
+
+    def create(ap,no,fe,lo,do,ge,ti,nu,te):
+        elemento = Responsable (apellido=ap, nombre=no, fecha_nac=fe, localidad_id=lo, domicilio=do, genero_id=ge, tipo_doc_id=ti, numero=nu, tel=te)
+        elemento.is_deleted = 0
+        db.session.add (elemento)
+        db.session.commit()
+        return elemento
